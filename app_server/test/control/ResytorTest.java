14322735c9a77e06bb19d4236b985626d3f4e4ad
luizventurote <luiz_17@ymail.com>
2014-09-04 15:59:25 -0300

    Testes adicionados

--- app_server/test/control/ResytorTest.java
+++ app_server/test/control/ResytorTest.java
@@ -49,5 +49,67 @@ public class ResytorTest {
         
         assertEquals(expResult, result);
     }
+            
+    /**
+     * Testa o método que calcula a frequência de uma palavra na string
+     */
+    @Test
+    public void test_removeStopWords() {
+        
+        System.out.println("Removendo strop words");
+        
+        String[] textos = new String[3];
+        int[] tamanhos = new int[3];
+        
+        // Frases
+        textos[0] = "Isso é um teste!";
+        tamanhos[0] = 2;
+        textos[1] = "Inflacao e um aumento generalizado de precos e resulta em perda do poder de compra.";
+        tamanhos[1] = 8;
+        textos[2] = "Sistemas Tradicionais de Arquivos foram substituidos pelos Sistemas de Bancos de Dados!";
+        tamanhos[2] = 9;
+        
+        // Variável para contar erros
+        int error = 0, qtd_sw=0;
+        
+        for(int i=0; i<tamanhos.length; i++) {
+            
+            // Remove as stopwords da string
+            textos[i] = Resytor.removeStopWords(textos[i]);
+            
+            // Quantidade de stopwords
+            qtd_sw = textos[i].split(" ").length;
+            
+            System.out.println("Qtd stopwords: "+qtd_sw+" - "+ textos[i]);
+            
+            if(qtd_sw != tamanhos[i]) {
+                error++;
+            }
+        }
+        
+        System.out.println("Quantidade de erros: "+error);
+        
+        assertEquals(0, error);
+
+        System.out.println("---------------------------------------");
+    }
+            
+    /**
+     * Testa a pesquisa por termos
+     */
+    @Test
+    public void test_pesquisarPorTermos() {
+        
+        System.out.println("Teste do método de pesquisa");
+       
+        Resytor resytor = new Resytor();
+        
+        resytor.pesquisarPorTermos("sistemas distribuidos e bancos de dados");
+        
+        System.out.println("---------------------------------------");
+        
+    }
+            
+       
     
 }
