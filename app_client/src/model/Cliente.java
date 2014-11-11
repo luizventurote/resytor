097ce73b65658cdca0c4b1da30ecd81c324747c0
Matheus Claudino <matheusclaudin@gmail.com>
2014-11-11 06:53:49 -0200

    comentarios removidos da classe cliente do chat

--- app_client/src/model/Cliente.java
+++ app_client/src/model/Cliente.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import java.io.BufferedReader;
@@ -11,35 +6,18 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 
-/**
- *
- * @author Marcelo
- */
 public class Cliente extends Thread {  
-  
-    // parte que controla a recepção de mensagens do cliente  
+    
     private Socket conexao;  
-  
-    // construtor que recebe o socket do cliente  
     public Cliente(Socket socket) {  
         this.conexao = socket;  
     }  
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        try {  
-  
-            // Instancia do atributo conexao do tipo Socket, conecta a IP do  
-            // Servidor, Porta  
-            //System.out.print("Socket valor: "+ InetAddress.getLocalHost().toString());
-            //Socket socket = new Socket(InetAddress.getLocalHost(),1234);// 8010);  
+        try {    
             Socket socket = new Socket("127.0.0.1",1500);// 8010);  
             
-            // Instancia do atributo saida, obtem os objetos que permitem  
-            // controlar o fluxo de comunicação  
             PrintStream saida = new PrintStream(socket.getOutputStream());  
             BufferedReader teclado = new BufferedReader(new InputStreamReader(  
                     System.in));  
@@ -49,69 +27,42 @@ public class Cliente extends Thread {
             // envia o nome digitado para o servidor  
             saida.println(meuNome.toUpperCase());  
   
-            // instancia a thread para ip e porta conectados e depois inicia ela  
             Thread thread = new Cliente(socket);  
             thread.start();  
-  
-            // Cria a variavel msg responsavel por enviar a mensagem para o  
-            // servidor  
+            
             String msg;  
             while (true) {  
-  
-                // cria linha para digitação da mensagem e a armazena na  
-                // variavel msg  
                 System.out.print("Mensagem > ");  
                 msg = teclado.readLine();  
-  
-                // envia a mensagem para o servidor  
                 saida.println(msg);  
             }  
         } catch (IOException e) {  
             System.out.println("Falha na Conexao... .. ." + " IOException: "  
                     + e);  
         }
-        
-        
-        
-    
-    }//fim main
+    }//main
     
     public void run() {  
         try {  
-  
             // recebe mensagens de outro cliente através do servidor  
             BufferedReader entrada = new BufferedReader(new InputStreamReader(  
                     this.conexao.getInputStream()));  
-  
-            // cria variavel de mensagem  
             String msg;  
             while (true) {  
-  
                 // pega o que o servidor enviou  
                 msg = entrada.readLine();  
-  
-                // se a mensagem contiver dados, passa pelo if, caso contrario  
-                // cai no break e encerra a conexao  
                 if (msg == null) {  
                     System.out.println("Conexão encerrada!");  
                     System.exit(0);  
                 }  
                 System.out.println();  
-  
-                // imprime a mensagem recebida  
                 System.out.println(msg);  
-  
-                // cria uma linha visual para resposta  
                 System.out.print("Responder > ");  
             }  
         } catch (IOException e) {  
-  
-            // caso ocorra alguma exceção de E/S, mostra qual foi.  
             System.out.println("Ocorreu uma Falha... .. ." + " IOException: "  
                     + e);  
         }  
-   }
-    
-    
+   }//run
     
 }//fim
