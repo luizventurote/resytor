77c946e8be88c372d8c719c5767e987fd02c0afa
Matheus Claudino <matheusclaudin@gmail.com>
2014-11-11 06:50:18 -0200

    comentarios removidos da classe servidor do chat

--- app_server/src/model/Servidor.java
+++ app_server/src/model/Servidor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import java.io.BufferedReader;
@@ -21,108 +16,67 @@ import java.util.Vector;
  * @author Marcelo
  */
 public class Servidor extends Thread {
-    
    
-    // Parte que controla as conexões por meio de threads.  
-    private static Vector CLIENTES;  
-  
-    // socket deste cliente  
-    private Socket conexao;  
-    
-    // nome deste cliente  
+    private static Vector CLIENTES;    
+    private Socket conexao;    
     private String nomeCliente;  
-    
-    // lista que armazena nome de CLIENTES  
     private static List LISTA_DE_NOMES = new ArrayList();  
   
-    // construtor que recebe o socket deste cliente  
     public Servidor(Socket socket) {  
         this.conexao = socket;  
     }  
   
-    // testa se nomes são iguais, se for retorna true  
-    public boolean armazena(String newName) {  
-  
-        // System.out.println(LISTA_DE_NOMES);  
+    public boolean armazena(String newName) {    
         for (int i = 0; i < LISTA_DE_NOMES.size(); i++) {  
             if (LISTA_DE_NOMES.get(i).equals(newName))  
                 return true;  
-        }  
-  
-        // adiciona na lista apenas se não existir  
+        }    
         LISTA_DE_NOMES.add(newName);  
         return false;  
     }  
   
-    // remove da lista os CLIENTES que já deixaram o chat  
     public void remove(String oldName) {  
         for (int i = 0; i < LISTA_DE_NOMES.size(); i++) {  
             if (LISTA_DE_NOMES.get(i).equals(oldName))  
                 LISTA_DE_NOMES.remove(oldName);  
         }  
     }  
-  
-
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
 
-      // instancia o vetor de CLIENTES conectados  
         CLIENTES = new Vector();  
         try {  
-  
-            // cria um socket que fica escutando a porta 5555.  
             ServerSocket server = new ServerSocket(1500);  
             System.out.println("ServidorSocket rodando na porta 1500");  
-            //System.out.println("ServidorSocket:"+server.toString());  
-      
-            // Loop principal.  
+            
             while (true) {  
-  
-                // aguarda algum cliente se conectar.  
-               // A execução do servidor fica bloqueada na chamada do método  
-                // accept da  
-                // classe ServerSocket até que algum cliente se conecte ao  
-                // servidor.  
-                // O próprio método desbloqueia e retorna com um objeto da  
-                // classe Socket  
                 Socket conexao = server.accept();  
   
-                // cria uma nova thread para tratar essa conexão  
                 Thread t = new Servidor(conexao);  
                 t.start();  
-                // voltando ao loop, esperando mais alguém se conectar.  
             }  
         } catch (IOException e) {  
-  
-           // caso ocorra alguma excessão de E/S, mostre qual foi.  
             System.out.println("IOException: " + e);  
         }  
-    }//fim main
+    }
   
-// execução da thread  
     @Override
     public void run() {  
-        try {  
-  
-            // objetos que permitem controlar fluxo de comunicação que vem do  
-            // cliente  
+        try {   
             BufferedReader entrada = new BufferedReader(new InputStreamReader(  
                     this.conexao.getInputStream()));  
             PrintStream saida = new PrintStream(this.conexao.getOutputStream());  
   
-           // recebe o nome do cliente  
+            // recebe o nome do cliente  
             this.nomeCliente = entrada.readLine();  
-  
-            // chamada ao metodo que testa nomes iguais  
+            
             if (armazena(this.nomeCliente)) {  
                 saida.println("Este nome ja existe! Conecte novamente com outro Nome.");  
                 CLIENTES.add(saida);  
-  
-                // fecha a conexao com este cliente  
+                
                 this.conexao.close();  
                 return;  
             } else {  
@@ -131,8 +85,6 @@ public class Servidor extends Thread {
                 System.out.println(this.nomeCliente  
                         + " : Conectado ao Servidor!");  
             }  
-  
-          // igual a null encerra a execução  
             if (this.nomeCliente == null) {  
                 return;  
             }  
@@ -141,43 +93,27 @@ public class Servidor extends Thread {
             CLIENTES.add(saida);  
   
             // recebe a mensagem do cliente  
-           String msg = entrada.readLine();  
-  
-            // Verificar se linha é null (conexão encerrada)  
-            // Se não for nula, mostra a troca de mensagens entre os CLIENTES  
+            String msg = entrada.readLine();  
             while (msg != null && !(msg.trim().equals(""))) {  
-  
-                // reenvia a linha para todos os CLIENTES conectados  
                 sendToAll(saida, " escreveu: ", msg);  
-  
-                // espera por uma nova linha.  
+   
                 msg = entrada.readLine();  
             }  
-  
-            // se cliente enviar linha em branco, mostra a saida no servidor  
-            System.out.println(this.nomeCliente + " saiu do bate-papo!");  
-  
-            // se cliente enviar linha em branco, servidor envia mensagem de  
-            // saida do chat aos CLIENTES conectados  
-            sendToAll(saida, " saiu", " do bate-papo!");  
-  
-            // remove nome da lista  
-            remove(this.nomeCliente);  
  
-            // exclui atributos setados ao cliente  
+            System.out.println(this.nomeCliente + " saiu do bate-papo!");  
+            sendToAll(saida, " saiu", " do bate-papo!");  
+            
+            remove(this.nomeCliente);  
             CLIENTES.remove(saida);  
-  
-            // fecha a conexao com este cliente  
+   
             this.conexao.close();  
         } catch (IOException e) {  
   
-            // Caso ocorra alguma excessão de E/S, mostre qual foi.  
             System.out.println("Falha na Conexao... .. ." + " IOException: "  
                     + e);  
         }  
     }  
   
-    // enviar uma mensagem para todos, menos para o próprio  
     public void sendToAll(PrintStream saida, String acao, String msg)  
             throws IOException {  
         Enumeration e = CLIENTES.elements();  
@@ -186,10 +122,9 @@ public class Servidor extends Thread {
             // obtém o fluxo de saída de um dos CLIENTES  
             PrintStream chat = (PrintStream) e.nextElement();  
   
-            // envia para todos, menos para o próprio usuário  
             if (chat != saida) {  
                 chat.println(this.nomeCliente + acao + msg);  
             }  
         }  
     }      
-}// fim servidor extends
+}
